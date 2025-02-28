8045b793184581b1def9c1784231b61877f0019e
Axel Pincheira <teck007@gmail.com>
2025-02-28 01:01:31 -0300

    add http response code

--- send-email.php
+++ send-email.php
@@ -6,7 +6,7 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 # Check environment variables
 if (!isset($_ENV['RESEND_API_KEY']) && !isset($_ENV['RESEND_EMAIL_FROM']) && !isset($_ENV['RESEND_EMAIL_TO'])) {
-  response('error', 'Missing environment variables');
+  response(500, 'error', 'Missing environment variables');
 }
 # Set constants
 define('RESEND_API_KEY', $_ENV['RESEND_API_KEY']);
@@ -20,14 +20,14 @@ header("Access-Control-Allow-Headers: Content-Type");
 # Check if the request origin is allowed
 if (isset($_SERVER['HTTP_ORIGIN'])) {
   if ($_SERVER['HTTP_ORIGIN'] !== FRONTEND_URL) {
-    response('error', 'Access denied (Invalid Origin)');
+    response(403, 'error', 'Access denied (Invalid Origin)');
   }
 } else {
-    response('error', 'No Origin header present');
+    response(400, 'error', 'No Origin header present');
 }
 # Check if the request method is POST
 if ($_SERVER['REQUEST_METHOD'] != 'POST') {
-  response('error', 'Invalid request method');
+  response(405, 'error', 'Invalid request method');
 }
 
 $name = $_POST['name'];
@@ -37,13 +37,13 @@ $message = $_POST['message'];
 function validateData($name, $email, $message)
 {
   if (empty($name) || empty($email) || empty($message)) {
-    response('error', 'All fields are required');
+    response(400, 'error', 'Missing data');
   }
   if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    response('error', 'Invalid email');
+    response(422, 'error', 'Invalid email');
   }
   if (strlen($name) > 100 || strlen($name) < 3 || strlen($email) > 100 || strlen($message) > 2000 || strlen($message) < 3) {
-    response('error', 'Invalid name, email or message');
+    response(400, 'error', 'Invalid data');
   }
 }
 # Send email using Resend
@@ -57,15 +57,19 @@ function sendEmail($name, $email, $message)
       'subject' => 'Mensaje de: ' . $name,
       'html' => '<p>' . $message . '</p><p>' . $email . '</p>'
     ]);
-    response('success', 'Email sent successfully');
+    response(200, 'success', 'Email sent successfully');
   } catch (Exception $e) {
-      response('error', 'Error sending email ' . $e->getMessage());
+      response(500, 'error', 'Error sending email: ' . $e->getMessage());
   }
 }
 # Return response
-function response($status, $data)
+function response($http_code=200 ,$status, $data)
 {
-  die(json_encode([$status => $data]));
+  header('Content-Type: application/json');
+  if ($http_code != 200) {
+    http_response_code($http_code);    
+  }
+  echo(json_encode([$status => $data]));
 }
 
 validateData($name, $email, $message);
